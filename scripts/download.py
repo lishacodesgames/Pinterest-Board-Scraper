cb26b33f3fcdb9d9799263535c5eabfb6615784e
Lisha from Linux-Mint-MATE-OLDHP <lishastudies.cs@gmail.com>
2026-06-12 12:14:20 +0530

    Added organisation

--- scripts/download.py
+++ scripts/download.py
@@ -15,20 +15,51 @@ def scrape(type: str) -> None:
    result = subprocess.run(command, text=True)
    print("Exit code for", type, "board:", result.returncode)
 
-def jpg_to_png(path: Path) -> None:
+def to_png(path: Path) -> Path:
+   png_path = path.with_suffix(".png")
    image = Image.open(path)
-   image.save(path.with_suffix(".png"))
+   image.save(png_path)
 
    path.unlink() # rm path
+   return png_path
+
+# returns true if png, false if gif
+def organise_by_suffix(img: Path, cache: Path) -> bool:
+   if(img.suffix.lower() == ".png"):
+      img.rename(cache / "PNGs" / img.name)
+      return True
+   elif(img.suffix.lower() == ".gif"):
+      img.rename(cache / "GIFs" / img.name)
+      return False
+   else:
+      raise TypeError("Only png and gif files must be organised! Cannot organise " + str(img))
 
 for type in ("editor", "sidebar", "panel"):
    # make cache directory and parents if needed, ignore if alr exists
    cache = CACHE_DIR / type
    cache.mkdir(parents=True, exist_ok=True) 
+   (cache / "GIFs").mkdir(exist_ok=True)
+   (cache / "PNGs").mkdir(exist_ok=True)
 
    scrape(type)
    
-   # convert jpg to png
+   print("\nOrganising subfolders...")
+   not_png_count = 0
+   png_count = 0
+   gif_count = 0
    for img in cache.iterdir():
-      if img.suffix.lower() in (".jpg", ".jpeg"):
-         jpg_to_png(img)
+      if not img.is_file():
+         continue
+
+      if img.suffix.lower() in (".jpg", ".jpeg", ".webp"):
+         not_png_count += 1
+         img = to_png(img)
+
+      if organise_by_suffix(img, cache):
+         png_count += 1
+      else:
+         gif_count += 1
+
+   print(not_png_count, "images converted to png.")
+   print(png_count, "PNGs moved to cache/" + type + "/PNGs")
+   print(gif_count, "GIFs moved to cache/" + type + "/GIFs\n")
